1550c94878274e7d7a398c4df26bdd8e7a16c304
Zoli <suveges.zoltan@petrik.hu>
2022-09-29 08:52:21 +0200

    Main.java -userInput

--- src/hu/petrik/harcosproject/Main.java
+++ src/hu/petrik/harcosproject/Main.java
@@ -1,7 +1,9 @@
 package hu.petrik.harcosproject;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     static List<Bejegyzes> bejegyzesek;
@@ -10,10 +12,28 @@ public class Main {
         bejegyzesek = new ArrayList<>();
         hozzaAd("Joe Doe", "Lorem Ipsum");
         hozzaAd("Jane Doe", "asd");
+        userInput();
+        for (Bejegyzes b : bejegyzesek) {
+            System.out.println(b);
+        }
     }
 
     public static void hozzaAd(String szerzo, String tartalom) {
         Bejegyzes b = new Bejegyzes(szerzo, tartalom);
         bejegyzesek.add(b);
+    }
+
+    public static void userInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Adja meg hány bejegyzés szeretne létrehozni:");
+        int numOfItem = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < numOfItem; i++) {
+            System.out.printf("\nAdja meg a(z) %d. szerzőt: ", i + 1);
+            String szerzo = sc.nextLine();
+            System.out.printf("\nAdja meg a(z) %d. bejegyzést: ", i + 1);
+            String tartalom = sc.nextLine();
+            hozzaAd(szerzo, tartalom);
+        }
     }
 }
