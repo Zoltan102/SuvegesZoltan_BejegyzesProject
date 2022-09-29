3c46401736b6d50d6204c73d374a0dd921b66d11
Zoli <suveges.zoltan@petrik.hu>
2022-09-29 08:39:02 +0200

    Main.java -hozzaAd -Lista -2 elem felvétele

--- src/hu/petrik/harcosproject/Main.java
+++ src/hu/petrik/harcosproject/Main.java
@@ -1,8 +1,19 @@
 package hu.petrik.harcosproject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    static List<Bejegyzes> bejegyzesek;
 
     public static void main(String[] args) {
+        bejegyzesek = new ArrayList<>();
+        hozzaAd("Joe Doe", "Lorem Ipsum");
+        hozzaAd("Jane Doe", "asd");
+    }
 
+    public static void hozzaAd(String szerzo, String tartalom) {
+        Bejegyzes b = new Bejegyzes(szerzo, tartalom);
+        bejegyzesek.add(b);
     }
 }
