bf9eec288052b549e0ff2c39724c73d64a5e1540
Zoli <suveges.zoltan@petrik.hu>
2022-09-29 08:18:31 +0200

    Bejegyzes.java -property-k -konstruktor -get/set-ek

---
+++ src/hu/petrik/harcosproject/Bejegyzes.java
@@ -0,0 +1,40 @@
+package hu.petrik.harcosproject;
+
+import java.time.LocalDateTime;
+
+public class Bejegyzes {
+    private String szerzo;
+    private String tartalom;
+    private int likeok;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
+
+    public Bejegyzes(String szerzo, String tartalom) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+    }
+
+    public String getSzerzo() {
+        return this.szerzo;
+    }
+
+    public String getTartalom() {
+        return this.tartalom;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+    }
+
+    public int getLikeok() {
+        return this.likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return this.letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return this.szerkesztve;
+    }
+}
